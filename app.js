73fe81b9d49688c43c6861de542899d40ff9cd08
lavanyack5 <69746818+lavanyack5@users.noreply.github.com>
2021-02-28 15:34:33 +0530

    2nd commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 350) {
+	if (scroll_position > 300) {
 		header.style.backgroundColor = '#29323c';
 	} else {
 		header.style.backgroundColor = 'transparent';
